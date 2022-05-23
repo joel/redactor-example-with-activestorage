01f3baf6d8a6170c931b1718916c93e219fe04ba
Joel Azemar <joel.azemar@gmail.com>
2022-05-23 15:15:58 +0200

    Configure Active Storage

--- config/application.rb
+++ config/application.rb
@@ -17,5 +17,7 @@ module RedactorExampleWithActivestorage
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+    config.active_storage.service = :local
   end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,6 +4,8 @@ ENV["RAILS_ENV"] ||= "test"
 require_relative "../config/environment"
 require "rails/test_help"
 
+Dir[Rails.root.join("test/support/**/*.rb")].sort.each { |f| require f }
+
 module ActiveSupport
   class TestCase
     # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
