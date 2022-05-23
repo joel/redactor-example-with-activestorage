0749269a569b7c0f8707936e1d33e0e5dd36d03c
Joel Azemar <joel.azemar@gmail.com>
2022-05-23 14:58:58 +0200

    Rubocop

--- app/models/note.rb
+++ app/models/note.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class Note < ApplicationRecord
   attribute :name, :string, default: -> { FFaker::Conference.name }
   validates :name, presence: true
--- db/schema.rb
+++ db/schema.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -10,13 +12,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_23_124429) do
-
+ActiveRecord::Schema.define(version: 20_220_523_124_429) do
   create_table "notes", force: :cascade do |t|
     t.string "name"
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
-
 end
